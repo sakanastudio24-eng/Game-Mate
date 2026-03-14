67cecd642e7ba1449963a1912190a00ef907bd12
sakanastudio24-eng <sakanastudio24@gmail.com>
2026-03-13 22:41:36 -0700

    fix(feed): restore top bar search and explain actions

--- app/(tabs)/news.tsx
+++ app/(tabs)/news.tsx
@@ -1348,6 +1348,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+    zIndex: 6,
+    elevation: 6,
   },
   topBarActions: {
     flexDirection: "row",
